eda6bdee8566450379939a99705269fb5fa755bd
Matthieu THIBAUT <matthieu.thibaut@edu.devinci.fr>
2022-01-16 20:06:22 +0100

    clean patterns

--- Vanilla/Patterns/index.js
+++ Vanilla/Patterns/index.js
@@ -8,17 +8,9 @@ const patternDict = [
         intent: 'Exit'
     },
     {
-        pattern: '\\b(?<weather>|weather|hot|cold|rain|rainy|sunny|snow|thunderstorms|windy|drizzle)?\\b\\s?in\\s\\b(?<city>[a-z]+[ a-z]+?)\\s?in?\\s?\\b(?<time>tomorrow|today|10\\sdays|5\\sdays)?$',
-        //pattern: '\\b(?<weather>|weather|hot|cold|rain|rainy|sunny|snow|thunderstorms|windy|drizzle)?\\b\\s\\b(?<time>day\\safter\\stomorrow|tomorrow|today)\\sin\\s\\b(?<city>[a-z]+[ a-z]+?)$',
-        //pattern : '\\b?(weather)\\s(like\\s)?in\\s\\b(?<city>[A-Za-z]+)\\s\\b(?<time>tomorrow|today)' ,
+        pattern: '\\b(?<weather>|weather|hot|cold|rain|rainy|cloudy)?\\b\\s?in\\s\\b(?<city>[a-z]+[ a-z]+?)\\s?in?\\s?\\b(?<time>tomorrow|today|10\\sdays|5\\sdays)?$',
         intent: 'Get Weather'
-    }//,
-    //{
-    //    pattern: 'in\\s\\b(?<city>[a-z]+[ a-z]+?)\\b',
-    //    //pattern : 'in\s\b(?<city>[A-Za-z]+)',
-    //    intent: 'Current Weather'
-    //}
+    }
 ];
 
 export default patternDict
-//module.exports = {patternDict};
